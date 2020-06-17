ccd8047ff8c52fdd3313eed70745c25d6eed211b
kanta-fujiwara <kanta.fujiwara@rakus.co.jp>
2020-06-17 11:41:15 +0900

    ログイン画面を表示できるようにした

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,15 @@
+package jp.co.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "administrator/login.html";
+  }
+
+}
