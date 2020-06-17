c1d5c63bf92c623bca0bbe43fa5b8e33a2869373
kanta-fujiwara <kanta.fujiwara@rakus.co.jp>
2020-06-17 12:15:21 +0900

    従業員情報追加ページを動作できるようにした
    insert.htmlのモックを埋めた
    InsertAdministratorFormを実装
    toInsert()を実装

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,11 +1,14 @@
 package jp.co.sample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jp.co.sample.form.InsertAdministratorForm;
 
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
+
 
   @RequestMapping("/")
   public String index() {
@@ -13,8 +16,15 @@ public class AdministratorController {
   }
 
   @RequestMapping("/toInsert")
-  public String toInsert() {
+  public String toInsert(Model model) {
+    model.addAttribute(new InsertAdministratorForm());
     return "administrator/insert";
   }
+
+  @RequestMapping("/insert")
+  public String insert(InsertAdministratorForm form) {
+    return "redirect:/";
+  }
+
 
 }
---
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -0,0 +1,45 @@
+package jp.co.sample.form;
+
+public class InsertAdministratorForm {
+  private String name;
+  private String mailAddress;
+  private String password;
+
+  public InsertAdministratorForm() {}
+
+  public InsertAdministratorForm(String name, String mailAddress, String password) {
+    this.name = name;
+    this.mailAddress = mailAddress;
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getMailAddress() {
+    return mailAddress;
+  }
+
+  public void setMailAddress(String mailAddress) {
+    this.mailAddress = mailAddress;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password="
+        + password + "]";
+  }
+}
